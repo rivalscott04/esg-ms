e752665d57ec67890042213a2c2dc91c2de0804a
ridwanali404 <ridwanali404@gmail.com>
2024-06-14 16:32:40 +0800

    update

--- resources/views/esg/create.blade.php
+++ resources/views/esg/create.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4 class="py-3 breadcrumb-wrapper mb-4">
-            <span class="text-muted fw-light">Master Data > </span>Pengguna
+            <span class="text-muted fw-light">Master Data > </span>ESG
         </h4>
         <div class="row">
             <div class="col-xl">
